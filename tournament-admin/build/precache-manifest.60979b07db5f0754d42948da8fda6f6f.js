3544b203cc16114f9f26445f2ae04cfaf0ac291b
KrasnovDaniil1 <63047682+KrasnovDaniil1@users.noreply.github.com>
2020-09-16 21:59:38 +0500

    Add files via upload

---
+++ tournament-admin/build/precache-manifest.60979b07db5f0754d42948da8fda6f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880ebb0eb4dba6ce554e9ade2e40ba45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a12b1b0c80bf896a366",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b834db4540db539c1b4",
+    "url": "/static/css/main.4c1b1219.chunk.css"
+  },
+  {
+    "revision": "0a12b1b0c80bf896a366",
+    "url": "/static/js/2.cb257be3.chunk.js"
+  },
+  {
+    "revision": "37958b17b727d67c0a2f063059df3b6f",
+    "url": "/static/js/2.cb257be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b834db4540db539c1b4",
+    "url": "/static/js/main.1d022662.chunk.js"
+  },
+  {
+    "revision": "a955c24da73e9573069b",
+    "url": "/static/js/runtime-main.2f1fd4f0.js"
+  }
+]);
